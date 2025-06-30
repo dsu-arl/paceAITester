4f3b240cf998f338eb4b0c8bc20584fb278aa1c0
Connor Ford <connorford23@gmail.com>
2025-06-30 11:08:09 -0500

    testing out sharing code between challenge verification scripts

--- verify_helpers.py
+++ verify_helpers.py
@@ -1,75 +1,75 @@
-#!/opt/pwn.college/python
+#!/usr/bin/exec-suid -- /usr/bin/python3.12 -I
 
 import ast
 
 
-def unparse(node):
-    """A backport of ast.unparse for Python 3.8."""
-    import ast
+# def unparse(node):
+#     """A backport of ast.unparse for Python 3.8."""
+#     import ast
 
-    class _Unparser(ast.NodeVisitor):
-        def __init__(self):
-            self.result = ""
+#     class unparser(ast.NodeVisitor):
+#         def __init__(self):
+#             self.result = ""
 
-        def visit_Module(self, node):
-            for stmt in node.body:
-                self.visit(stmt)
+#         def visit_Module(self, node):
+#             for stmt in node.body:
+#                 self.visit(stmt)
 
-        def visit_Expr(self, node):
-            self.visit(node.value)
-            self.result += "\n"
+#         def visit_Expr(self, node):
+#             self.visit(node.value)
+#             self.result += "\n"
 
-        def visit_Assign(self, node):
-            for target in node.targets:
-                self.visit(target)
-                self.result += " = "
-            self.visit(node.value)
-            self.result += "\n"
+#         def visit_Assign(self, node):
+#             for target in node.targets:
+#                 self.visit(target)
+#                 self.result += " = "
+#             self.visit(node.value)
+#             self.result += "\n"
 
-        def visit_Name(self, node):
-            self.result += node.id
+#         def visit_Name(self, node):
+#             self.result += node.id
 
-        def visit_Constant(self, node):
-            self.result += repr(node.value)
+#         def visit_Constant(self, node):
+#             self.result += repr(node.value)
 
-        def visit_BinOp(self, node):
-            self.visit(node.left)
-            self.result += f" {self.operator(node.op)} "
-            self.visit(node.right)
+#         def visit_BinOp(self, node):
+#             self.visit(node.left)
+#             self.result += f" {self.operator(node.op)} "
+#             self.visit(node.right)
 
-        def visit_Attribute(self, node):
-            # Handles attribute access like model.fit
-            self.visit(node.value)  # Visit the base (e.g., model)
-            self.result += f".{node.attr}"  # Append the attribute name (e.g., fit)
+#         def visit_Attribute(self, node):
+#             # Handles attribute access like model.fit
+#             self.visit(node.value)  # Visit the base (e.g., model)
+#             self.result += f".{node.attr}"  # Append the attribute name (e.g., fit)
 
-        def visit_Call(self, node):
-            # Handles function/method calls like model.fit()
-            self.visit(node.func)  # Visit the function being called
-            self.result += "("
-            for i, arg in enumerate(node.args):
-                if i > 0:
-                    self.result += ", "
-                self.visit(arg)  # Visit each argument
-            self.result += ")"
+#         def visit_Call(self, node):
+#             # Handles function/method calls like model.fit()
+#             self.visit(node.func)  # Visit the function being called
+#             self.result += "("
+#             for i, arg in enumerate(node.args):
+#                 if i > 0:
+#                     self.result += ", "
+#                 self.visit(arg)  # Visit each argument
+#             self.result += ")"
 
-        def visit_arguments(self, node):
-            # Handle arguments for function definitions (not relevant here)
-            pass
+#         def visit_arguments(self, node):
+#             # Handle arguments for function definitions (not relevant here)
+#             pass
 
-        def operator(self, op):
-            if isinstance(op, ast.Add):
-                return "+"
-            if isinstance(op, ast.Sub):
-                return "-"
-            if isinstance(op, ast.Mult):
-                return "*"
-            if isinstance(op, ast.Div):
-                return "/"
-            raise NotImplementedError(f"Operator {op} not implemented")
+#         def operator(self, op):
+#             if isinstance(op, ast.Add):
+#                 return "+"
+#             if isinstance(op, ast.Sub):
+#                 return "-"
+#             if isinstance(op, ast.Mult):
+#                 return "*"
+#             if isinstance(op, ast.Div):
+#                 return "/"
+#             raise NotImplementedError(f"Operator {op} not implemented")
 
-    unparser = _Unparser()
-    unparser.visit(node)
-    return unparser.result
+#     unparser = _unparser()
+#     unparser.visit(node)
+#     return unparser.result
 
 
 def extract_python_details(filepath):
@@ -82,16 +82,16 @@ def extract_python_details(filepath):
         if isinstance(node, ast.Assign) and isinstance(node.value, ast.Call):
             # Handle unpacked variables (tuple or list)
             if isinstance(node.targets[0], (ast.Tuple, ast.List)):
-                variable = tuple(unparse(var) for var in node.targets[0].elts)
+                variable = tuple(ast.unparse(var) for var in node.targets[0].elts)
             # Single variable assignment
             elif isinstance(node.targets[0], ast.Name):
                 variable = node.targets[0].id if node.targets[0].id != '_' else None
             else:
                 variable = None
             
-            func = unparse(node.value.func)
-            args = [unparse(arg) for arg in node.value.args]
-            kwargs = {kw.arg: unparse(kw.value) for kw in node.value.keywords}
+            func = ast.unparse(node.value.func)
+            args = [ast.unparse(arg) for arg in node.value.args]
+            kwargs = {kw.arg: ast.unparse(kw.value) for kw in node.value.keywords}
 
             lines.append({
                 'variable': variable,
@@ -102,9 +102,9 @@ def extract_python_details(filepath):
 
         # Handle standalone function calls like model.fit(...)
         elif isinstance(node, ast.Expr) and isinstance(node.value, ast.Call):
-            func = unparse(node.value.func)
-            args = [unparse(arg) for arg in node.value.args]
-            kwargs = {kw.arg: unparse(kw.value) for kw in node.value.keywords}
+            func = ast.unparse(node.value.func)
+            args = [ast.unparse(arg) for arg in node.value.args]
+            kwargs = {kw.arg: ast.unparse(kw.value) for kw in node.value.keywords}
 
             lines.append({
                 'variable': None,
